f9a6590de0eb85a715ffa89811f894f1c1e7eb10
Werick Caio <wericcaiodecastro@gmail.com>
2025-11-28 22:26:49 -0300

    Hardware: adição do comentário para o começo do desenvolvimento de hardware

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include <Arduino.h>
+// Começar a definição  do uso da comunicação que temos hoje no próprio painel LIVE, e ver uma maneira de fazer funcinoar com o ESP32
+
+// Vou fazer o primeiro commit só com isso mesmo, pois será uma tarefa para outro dia terminar isso
 
 // Definição do pino do LED (No ESP32 DevKit V1 geralmente é o pino 2)
 #define LED_BUILTIN 2
@@ -6,6 +9,7 @@
 void setup() {
   // Configura o pino como saída
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(5, OUTPUT);
   
   // Inicia a Serial para debug básico
   Serial.begin(115200);
